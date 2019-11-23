9c2fa8bc1be9419e1d09ac417c7d5c693f4da4c7
dani <daniel.avila@electronicid.eu>
2019-11-23 21:59:25 +0100

    Characters list

--- app/src/main/java/avila/daniel/rickmorty/ui/CharactersLocationActivity.kt
+++ app/src/main/java/avila/daniel/rickmorty/ui/CharactersLocationActivity.kt
@@ -1,13 +1,46 @@
 package avila.daniel.rickmorty.ui
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import avila.daniel.rickmorty.R
 import avila.daniel.rickmorty.base.BaseActivity
+import avila.daniel.rickmorty.ui.fragment.characters.CharactersAdapter
+import avila.daniel.rickmorty.ui.model.CharacterUI
+import avila.daniel.rickmorty.ui.util.data.ResourceState
+import kotlinx.android.synthetic.main.activity_characters.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class CharactersLocationActivity : BaseActivity() {
+
+    private val charactersLocationViewModel: CharactersLocationViewModel by viewModel()
+
+    private val characterList = mutableListOf<CharacterUI>()
+    private val adapter = CharactersAdapter(characterList)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setListener()
+
+        list_characters.adapter = adapter
+
+        intent.extras?.run {
+
+        }
+    }
+
+    private fun setListener() {
+        charactersLocationViewModel.charactersLiveData.observe(this, Observer { resource ->
+            resource?.run {
+                managementResourceState(status, message)
+                if (status == ResourceState.SUCCESS) {
+                    data?.run {
+                        characterList.addAll(this)
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        })
     }
 
     override fun getLayoutId(): Int = R.layout.activity_characters
--- app/src/main/java/avila/daniel/rickmorty/ui/CharactersLocationViewModel.kt
+++ app/src/main/java/avila/daniel/rickmorty/ui/CharactersLocationViewModel.kt
@@ -6,6 +6,7 @@ import avila.daniel.domain.interactor.GetLocationCharactersUseCase
 import avila.daniel.rickmorty.base.BaseViewModel
 import avila.daniel.rickmorty.schedulers.IScheduleProvider
 import avila.daniel.rickmorty.ui.model.CharacterUI
+import avila.daniel.rickmorty.ui.model.CharactersSource
 import avila.daniel.rickmorty.ui.model.mapper.CharacterUIMapper
 import avila.daniel.rickmorty.ui.util.data.Resource
 import avila.daniel.rickmorty.util.SingleLiveEvent
@@ -16,11 +17,25 @@ class CharactersLocationViewModel(
     private val getEpisodeCharactersUseCase: GetEpisodeCharactersUseCase,
     private val characterUIMapper: CharacterUIMapper,
     private val scheduleProvider: IScheduleProvider
-    ): BaseViewModel() {
+) : BaseViewModel() {
+
     val charactersLiveData = SingleLiveEvent<Resource<List<CharacterUI>>>()
 
-    fun loadCharacters(){
+    fun loadCharacters(charactersSource: CharactersSource, id: Int) {
+        charactersLiveData.value = Resource.loading()
+        addDisposable(
+            when (charactersSource) {
+                CharactersSource.EPISODE -> getEpisodeCharactersUseCase.execute(id)
+                CharactersSource.LOCATION -> getLocationCharactersUseCase.execute(id)
+            }
+                .observeOn(scheduleProvider.ui())
+                .subscribeOn(scheduleProvider.io())
+                .subscribe({ characters ->
+                    charactersLiveData.value = Resource.success(characterUIMapper.map(characters))
 
+                }) {
+
+                    charactersLiveData.value = Resource.error(it.localizedMessage)
+                })
     }
-
 }

c6e7cf98fc2738778adf544bfaaca05069e06aef
dani <daniel.avila@electronicid.eu>
2019-11-30 12:56:14 +0100

    refactor menu

--- app/src/main/java/avila/daniel/rickmorty/ui/MainActivity.kt
+++ app/src/main/java/avila/daniel/rickmorty/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package avila.daniel.rickmorty.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -12,6 +14,7 @@ import avila.daniel.rickmorty.base.BaseActivity
 import avila.daniel.rickmorty.ui.fragment.characters.CharactersFragment
 import avila.daniel.rickmorty.ui.fragment.episodes.EpisodesFragment
 import avila.daniel.rickmorty.ui.fragment.locations.LocationsFragment
+import avila.daniel.searchview_test.SimpleSearchView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
@@ -40,56 +43,56 @@ class MainActivity : BaseActivity() {
 
         tabs.setupWithViewPager(viewpager)
 
-//        search_view.tabLayout = tabs
+        search_view.tabLayout = tabs
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // Handle action bar item clicks here. The action bar will
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
+            R.id.action_info -> {
+                Toast.makeText(this, "Info", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_toolbar, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean { // Handle action bar item clicks here. The action bar will
-        val id: Int = item.itemId
-        if (id == R.id.action_settings) {
-            Toast.makeText(this@MainActivity, "Action clicked", Toast.LENGTH_LONG).show()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
+        search_view.setMenuItem(menu.findItem(R.id.action_search))
+        search_view.setOnQueryTextListener(object : SimpleSearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Log.d("fff", query)
+                return false
+            }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        menuInflater.inflate(R.menu.menu_toolbar, menu)
-//
-//        search_view.setMenuItem(menu.findItem(R.id.action_search))
-//
-//        search_view.setOnQueryTextListener(object : SimpleSearchView.OnQueryTextListener {
-//            override fun onQueryTextSubmit(query: String): Boolean {
-//                Log.d("fff", query)
-//                return false
-//            }
-//
-//            override fun onQueryTextChange(newText: String): Boolean {
-//                Log.d("fff", "cambio")
+            override fun onQueryTextChange(newText: String): Boolean {
+                Log.d("fff", "cambio")
 //                fragmentTabs[viewpager.currentItem].updateCriteria(newText)
-//                return false
-//            }
-//
-//            override fun onQueryTextCleared(): Boolean {
-//
-//                return false
-//            }
-//
-//        })
-//
-//        return super.onCreateOptionsMenu(menu)
-//    }
-//
-//    override fun onBackPressed() {
-//        if (search_view.onBackPressed()) {
-//            return
-//        }
-//        super.onBackPressed()
-//    }
+                return false
+            }
+
+            override fun onQueryTextCleared(): Boolean {
+
+                return false
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onBackPressed() {
+        if (search_view.onBackPressed()) {
+            return
+        }
+        super.onBackPressed()
+    }
 
     override fun getLayoutId(): Int = R.layout.activity_main
 
--- app/src/main/java/avila/daniel/rickmorty/ui/SettingsActivity.kt
+++ app/src/main/java/avila/daniel/rickmorty/ui/SettingsActivity.kt
@@ -1,19 +1,33 @@
-package avila.daniel.rickmorty
+package avila.daniel.rickmorty.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import avila.daniel.rickmorty.R
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
+        setContentView(R.layout.activity_settings)
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.settings, SettingsFragment())
+            .replace(
+                R.id.settings,
+                SettingsFragment()
+            )
             .commit()
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {

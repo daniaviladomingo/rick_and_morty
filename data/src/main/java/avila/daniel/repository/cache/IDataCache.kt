de2d4e4102484a66b66900c5f7aa2cd0b906a50f
UltraBook <daniaviladomingo@gmail.com>
2019-12-01 22:16:56 +0100

    cache +

--- data/src/main/java/avila/daniel/repository/cache/IDataCache.kt
+++ data/src/main/java/avila/daniel/repository/cache/IDataCache.kt
@@ -1,4 +1,12 @@
 package avila.daniel.repository.cache
 
+import avila.daniel.domain.model.settings.CharactersFilterSettings
+import avila.daniel.domain.model.settings.EpisodeFilterSettings
+import avila.daniel.domain.model.settings.LocationFilterSettings
+import io.reactivex.Single
+
 interface IDataCache {
+    fun getCharacterFilter(): Single<CharactersFilterSettings>
+    fun getLocationFilter(): Single<LocationFilterSettings>
+    fun getEpisodeFilter(): Single<EpisodeFilterSettings>
 }
--- data_cache_preference/src/main/java/avila/daniel/data_cache_preference/DataCachePreferenceImp.kt
+++ data_cache_preference/src/main/java/avila/daniel/data_cache_preference/DataCachePreferenceImp.kt
@@ -1,10 +1,33 @@
 package avila.daniel.data_cache_preference
 
 import android.content.SharedPreferences
+import avila.daniel.domain.model.settings.CharactersFilterSettings
+import avila.daniel.domain.model.settings.EpisodeFilterSettings
+import avila.daniel.domain.model.settings.LocationFilterSettings
 import avila.daniel.repository.cache.preference.IDataCachePreference
+import io.reactivex.Single
 
 class DataCachePreferenceImp(
-    private val sharedPreferences: SharedPreferences
+    private val sharedPreferences: SharedPreferences,
+    private val keyNameCharacter: String,
+    private val keyStatus: String,
+    private val keySpecie: String,
+    private val keyTypeCharacter: String,
+    private val keyGender: String,
+    private val keyNameLocation: String,
+    private val keyTypeLocation: String,
+    private val keyNameEpisode: String
 ) : IDataCachePreference {
+    override fun getCharacterFilter(): Single<CharactersFilterSettings> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLocationFilter(): Single<LocationFilterSettings> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getEpisodeFilter(): Single<EpisodeFilterSettings> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
